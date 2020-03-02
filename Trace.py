57568282783144fbaa51b3a750c4f96d849a9845
Daniel Haden <haden.daniel@gmail.com>
2020-03-02 15:17:35 -0700

    Used inheritance for UDP and ICMP probes

--- Trace.py
+++ Trace.py
@@ -1,11 +1,15 @@
 from scapy.all import *
 import socket
 
-class ICMPTrace:
+class Trace:
     probeDetails = {}
+    layer4protocol = None
 
     def __init__(self, maxTTL):
         self.maxTTL = maxTTL
+
+    def probe_length(self):
+        return len(self.probeDetails)
 
     def get_hostname(self, ip):
         try:
@@ -13,12 +17,11 @@ class ICMPTrace:
         except:
             return None
 
-
     def probe(self, target):
         target_ip = socket.gethostbyname(target)
 
         for i in range(1, self.maxTTL):
-            pkt = IP(dst=target, ttl=i) / ICMP()
+            pkt = IP(dst=target, ttl=i) / self.layer4protocol
             reply = sr1(pkt, verbose=0)
 
             if reply is None:
@@ -30,11 +33,15 @@ class ICMPTrace:
 
         return False
 
-    def probe_length(self):
-        return len(self.probeDetails)
-
     def show_traceroute(self, target):
         for i in range(1, self.maxTTL):
-            pkt = IP(dst=target, ttl=i) / ICMP()
+            pkt = IP(dst=target, ttl=i) / self.layer4protocol
             reply = sr1(pkt, verbose=0)
             print(i,'\t\t',reply.src, '\t\t', self.get_hostname(reply.src))
+
+
+class ICMPTrace(Trace):
+    layer4protocol = ICMP()
+
+class UDPTrace(Trace):
+    layer4protocol = UDP()
