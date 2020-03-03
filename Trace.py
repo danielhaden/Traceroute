c16490323124951bcf8cc4b78140d3de5b941331
Daniel Haden <haden.daniel@gmail.com>
2020-03-02 19:35:58 -0700

    Folded traceroute output into probe method

--- Trace.py
+++ Trace.py
@@ -17,28 +17,35 @@ class Trace:
         except:
             return None
 
-    def probe(self, target):
+    def probe(self, target, verbose=False):
         target_ip = socket.gethostbyname(target)
 
         for i in range(1, self.maxTTL):
-            pkt = IP(dst=target, ttl=i) / self.layer4protocol
-            reply = sr1(pkt, verbose=0)
+            ## set src because scapy will not use current IP when using automated VPN vantage switching
+            pkt = IP(dst=target_ip, ttl=i) / self.layer4protocol
+
+            reply = sr1(pkt, verbose=0, timeout=4)
 
             if reply is None:
                 break
             elif reply.src == target_ip:
+                if verbose:
+                    print("******************************")
                 return True
             else:
+                ## should add hostname method later
                 self.probeDetails[reply.src] = None
 
+                if verbose:
+                    if i == 1:
+                        print('HOP', '\t', "ROUTER IP", '\t\t\t', "HOSTNAME")
+
+                    print(i, '\t\t', reply.src, '\t\t', self.get_hostname(reply.src))
+
+        if verbose:
+            print("***DID NOT REACH TARGET***")
+
         return False
-
-    def show_traceroute(self, target):
-        for i in range(1, self.maxTTL):
-            pkt = IP(dst=target, ttl=i) / self.layer4protocol
-            reply = sr1(pkt, verbose=0)
-            print(i,'\t\t',reply.src, '\t\t', self.get_hostname(reply.src))
-
 
 class ICMPTrace(Trace):
     layer4protocol = ICMP()
